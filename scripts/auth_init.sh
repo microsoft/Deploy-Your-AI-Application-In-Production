8d739fff155a7131c9b429855f70f02a17d75720
Prajwal-Microsoft <v-dcprajwal@microsoft.com>
2025-07-28 18:12:44 +0530

    fix: Handle Null values for Sample App (#71)

--- scripts/auth_init.sh
+++ scripts/auth_init.sh
@@ -1,10 +1,12 @@
- #!/bin/sh
+#!/bin/bash
 
+# Load environment variables from a shell script
 . ./scripts/loadenv.sh
 
-if [ "$AZURE_APP_SAMPLE_ENABLED" = "false" ]; then
-    echo "AZURE_APP_SAMPLE_ENABLED is false. Exiting auth_init script."
-    exit 0
+# Check if AZURE_APP_SAMPLE_ENABLED is not set or is "false"
+if [[ -z "$AZURE_APP_SAMPLE_ENABLED" || "$AZURE_APP_SAMPLE_ENABLED" == "false" ]]; then
+  echo "AZURE_APP_SAMPLE_ENABLED is false. Exiting auth_init script."
+  exit 0
 fi
 
 echo 'Running "auth_init.py"'
